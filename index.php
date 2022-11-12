ae769912de2a03fbb1c3da4de94f69e547d7c9d0
Ribeiro <lucasribeiro1024b@gmail.com>
2022-11-12 10:43:39 -0300

    fixed array position for route /cats

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $response;
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ((isset($uri[1]) && $uri[1] != "cats") || !isset($uri[2])) {
+if ((isset($uri[2]) && $uri[2] != "cats") || !isset($uri[2])) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
