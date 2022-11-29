8bc5ed5dd2583ec7e68ab662828633754de275f0
ribeiro <lucasribeiro1024b@gmail.com>
2022-11-28 21:50:20 -0300

    update

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ $response = [];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ((isset($uri[2]) && $uri[2] != "cats") || !isset($uri[2], $uri[3])) {
+if ((isset($uri[2]) && $uri[2] != "cats") || !isset($uri[2], $uri[3]))
+{
     header("HTTP/1.1 404 Not Found");
     exit();
 }
-else {
+else
+{
     switch ($uri[3]) {
         /**
          * /cats/list
